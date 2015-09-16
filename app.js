272d61a36a005e62cd88485b32dd983c92822df4
Bryan Duplantis <duplantis@gmail.com>
2015-09-16 17:45:16 -0500

    set up Angular, routes

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ app.set('view engine', 'ejs');
 
 require('./lib/mongod');
 
-app.get('/', function (req, res) {
-  res.render('templates/childcare', {title: "Jazzercise"});
+app.use(express.static('www'));
+
+app.post('/childcare/data', function (req, res) {
+  // 1. Does some model logic
+
+  // 2. Returns data to front-end
+  res.send({title: 'Jazzercise'});
 });
 
 // errors
 app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
 });
-
-app.use(express.static('www'));
 
 // pass 4 arguments to create an error handling middleware
 app.use(function (req, res) {
